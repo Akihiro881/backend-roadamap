f4ff2c8cdc9ff45841da58a7ffc3b812d79022c1
akihiro <tuyoengineer@gmail.com>
2026-09-13 03:36:14 +0900

    enum,Error,custom-error,generics,strctEmbedding, tange-over

---
+++ phase1/generics.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+// [S ~[]E, E comparable]	この関数で使う「型」を、呼び出し時に合わせて決める宣言(型パラメータ)
+// (s S, v E)	普通の引数(型はさっき宣言したS, Eを使う)
+func SlicesIndex[S ~[]E, E comparable](s S, v E) int {
+	for i := range s {
+		if v == s[i] {
+			return i
+		}
+	}
+	return -1
+}
+
+type List[T any] struct {
+	head, tail *element[T]
+}
+
+type element[T any] struct {
+	next *element[T]
+	val  T
+}
+
+func (lst *List[T]) Push(v T) {
+	if lst.tail == nil {
+		lst.head = &element[T]{val: v}
+		lst.tail = lst.head
+	} else {
+		lst.tail.next = &element[T]{val: v}
+		lst.tail = lst.tail.next
+	}
+}
+
+func (lst *List[T]) AllElements() []T {
+	var elems []T
+	for e := lst.head; e != nil; e = e.next {
+		elems = append(elems, e.val)
+	}
+	return elems
+}
+
+func main() {
+	var s = []string{"foo", "bar", "zoo"}
+	fmt.Println("index of zoo:", SlicesIndex(s, "zoo"))
+
+	_ = SlicesIndex[[]string, string](s, "zoo")
+
+	lst := List[int]{}
+	lst.Push(10)
+	lst.Push(13)
+	lst.Push(23)
+	fmt.Println("list:", lst.AllElements())
+}
